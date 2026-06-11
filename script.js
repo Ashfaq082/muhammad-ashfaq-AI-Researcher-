03539ad11661379472a8db52965dee3b80ff98d6
Ashfaq082 <muhammad.ashfaq@uettaxila.edu.pk>
2026-06-11 22:16:26 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll("section").forEach(sec=>{
+    observer.observe(sec);
+});
